89004b468db56c8e6782f0769087b86a9a150c71
DritonCa <Dritoncazimi@gmail.com>
2024-04-03 03:51:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const { scrapeLogic } = require("./scrapeLogic");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/scrape", (req, res) =>{
+    scrapeLogic(res);
+});
+
+app.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`);
+})
