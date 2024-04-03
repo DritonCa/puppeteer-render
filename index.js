1a698d3b2b7d4aa9f9b1f023f6c5a2dc60978dd7
Driton Cazimi <dritoncazimi@gmail.com>
2024-04-03 05:04:06 +0200

    added logic for handling json

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) =>{
-    scrapeLogic(res);
-});
-
 app.post("/scrape", (req, res) =>{
+    console.log(req.body);
     scrapeLogic(req,res);
 });
 
