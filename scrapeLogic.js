0dcf2fb9286d582cf66154132c1b305e83886a85
Driton Cazimi <dritoncazimi@gmail.com>
2024-04-05 21:58:32 +0200

    added logic to handle undified

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -35,8 +35,8 @@ const scrapeLogic = async (req,res) => {
             // Extract data from elements with specified classes
             const englishData = $('.englishcontainer').text().trim();
             const arabicData = $('.arabic_hadith_full').text().trim();
-            const englishGrade = $('.english_grade').text().trim();
-            const arabicGrade = $('.arabic_grade').text().trim();
+            
+            
             const reference = $('.hadith_reference').text().trim();
 
 
@@ -45,8 +45,8 @@ const scrapeLogic = async (req,res) => {
             const narrattor = splitEnglishData[0].trim();
             const narration = splitEnglishData[1].trim();
 
-            const trimmedArabicGrade = arabicGrade.replace(/\u062d\u0643\u0645\s*:/, '').trim();
-
+            const englishGrade = $('.english_grade').text()?.trim();
+            if (englishGrade !== null && englishGrade !== undefined) {
             const match = englishGrade.match(/Grade:\s*([^()]+)\s*\(([^)]+)\)/);
 
             let grade;
@@ -59,7 +59,11 @@ const scrapeLogic = async (req,res) => {
                 // If no match found, use the original string as the grade
                 grade = englishGrade;
             }
+            }
 
+            const arabicGrade = $('.arabic_grade').text()?.trim();
+            if (arabicGrade !== null && arabicGrade !== undefined) {
+            const trimmedArabicGrade = arabicGrade.replace(/\u062d\u0643\u0645\s*:/, '').trim();
             const matchArabic = trimmedArabicGrade.match(/^([^()]+)\s*\(([^)]+)\)$/);
 
             let gradeArabic;
@@ -73,6 +77,7 @@ const scrapeLogic = async (req,res) => {
                 gradeArabic = trimmedArabicGrade.trim();
                 commentator = "";
             }
+        }
 
 
             // Split the text into an array based on "In-book reference" and "English translation"
@@ -81,7 +86,6 @@ const scrapeLogic = async (req,res) => {
             // Trim each part to remove leading and trailing whitespace
             const references = parts[0].trim().replace(/^Reference\s*:\s*/, ''); // Remove "Reference :" prefix
             const inBookReference = parts[1].trim();
-            const englishTranslation = parts[2].trim();
 
             // Construct and return an object with filtered data
             return {
